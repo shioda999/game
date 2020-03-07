6e269f7e6e9cbb890f0dd86586be7b210fe03d20
shioyakioh <k0shig0e17@gmail.com>
2020-03-07 23:26:54 +0900

    分や時間で表示するように変更。

--- work/stopwatch/stopwatch.js
+++ work/stopwatch/stopwatch.js
@@ -1,9 +1,20 @@
 let start = 0, end = 0, loopFlag = false
+const printTime = (time) => {
+    let s = Math.floor(time / 1000)
+    let m = Math.floor(s / 60)
+    let h = Math.floor(m / 60)
+    let d = Math.floor(h / 24)
+    let str = ""
+    if(d)str += d + "日 "
+    if(h)str += h % 24 + "時 "
+    if(m)str += m % 60 + "分 "
+    return str + s % 60 + "秒" + Math.floor(time) % 1000
+}
 const timerUpdate = () => {
     if(loopFlag){
         setTimeout(timerUpdate, 1)
         end = performance.now()
-        document.querySelector('#timer').innerText = "経過時間: " + (end - start).toFixed(0) + "ms"
+        document.querySelector('#timer').innerText = "経過時間: " + printTime(end - start)
     }
 }
 const clickFunc = () => {
@@ -22,5 +33,5 @@ const clickFunc = () => {
 const resetFunc = () => {
     loopFlag = false
     start = end = 0
-    document.querySelector('#timer').innerText = "経過時間: 0ms"
+    document.querySelector('#timer').innerText = "経過時間: 0秒000"
 }
